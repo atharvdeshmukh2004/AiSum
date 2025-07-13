1d7141d7032d00684c685eb806790e78facc6b24
atharvdeshmukh2004 <datharv56@gmail.com>
2025-07-14 00:47:20 +0530

    Update firebaseConfig.ts

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -7,19 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDi6l4fOSvAVpGLdQWZijRDcz80vCA0g2A",
-  authDomain: "aisum-92ead.firebaseapp.com ",
-  projectId: "aisum-92ead",
-  storageBucket: "aisum-92ead.firebasestorage.app",
-  messagingSenderId: "492241802551",
-  appId: " 1:492241802551:web:d21e97731a7670293bb6b2",
-
-  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ,
-  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ,
-  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ,
-  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ,
-  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ,
-  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
